51ea194e726ad3dd17e100a4eaddea1ecb8668cc
root <eldermickelson@yahoo.com>
2018-02-06 05:57:33 +0000

    added reverse, broke movement into functions

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ while (joystick_count < 1):
         
 #Initialize Global Variables
 loop = True
-repeatHZ = 0
-repeatVT = 0
 axesHZ = 0
 axesVT = 0
+repeatHZ = 0
+repeatVT = 0
 
 #Set Motor Pin numbers
 myMotorLT = mh.getMotor(3)
@@ -31,44 +31,73 @@ for i in range (joystick_count):
     joystick = pygame.joystick.Joystick(i)
     joystick.init()
 
+
+def forward(axesVT, axesHZ):
+
+    axesVT = round((axesVT * float(-150)))
+    if (axesVT > 0):
+        try:
+            myMotorRT.run(Adafruit_MotorHAT.FORWARD)
+            myMotorLT.run(Adafruit_MotorHAT.FORWARD)
+        except OSError:
+            mh = Adafruit_MotorHAT(addr=0x60)
+            print ("error error error error error")
+        axesMotorRT = axesVT
+        axesMotorLT = axesVT
+        if (axesHZ > 0):
+            axesMotorRT = (axesVT *(1- axesHZ))
+            print('right')
+        elif (axesHZ < 0):
+            axesMotorLT = (axesVT *(1- (axesHZ*(-1))))
+            print('left')
+        try:
+            myMotorLT.setSpeed(int(axesMotorLT))
+            myMotorRT.setSpeed(int(axesMotorRT))
+        except OSError:
+            mh = Adafruit_MotorHAT(addr=0x60)
+            print ("error error error error error")
+    elif (axesVT == 0 and axesHZ ==0):
+        myMotorRT.run(Adafruit_MotorHAT.RELEASE);
+        myMotorLT.run(Adafruit_MotorHAT.RELEASE);
+        print ("release")
+           # print ("LEFT: ", axesMotorLT)
+           # print ("RIGHT: ", axesMotorRT)
+
+def reverse(axesVT, axesHZ):
+
+    if (axesVT > 0):
+        try:
+            myMotorRT.run(Adafruit_MotorHAT.BACKWARD)
+            myMotorLT.run(Adafruit_MotorHAT.BACKWARD)
+        except OSError:
+            mh = Adafruit_MotorHAT(addr=0x60)
+            print ("error error error error error")
+        axesMotorRT = axesVT
+        axesMotorLT = axesVT
+        if (axesHZ > 0):
+            axesMotorRT = (axesVT *(1- axesHZ))
+            print('reverse right')
+        elif (axesHZ < 0):
+            axesMotorLT = (axesVT *(1- (axesHZ*(-1))))
+            print('reverse left')
+        try:
+            myMotorLT.setSpeed(int(axesMotorLT))
+            myMotorRT.setSpeed(int(axesMotorRT))
+        except OSError:
+            mh = Adafruit_MotorHAT(addr=0x60)
+            print ("error error error error error")
+           # print ("LEFT: ", axesMotorLT)
+           # print ("RIGHT: ", axesMotorRT)
+
 while (loop == True):
     axesVT = joystick.get_axis(1)
     axesHZ = joystick.get_axis(0)
     pygame.event.pump()
-    axesVT = round((axesVT * float(-150)))
     if (repeatHZ != axesHZ or repeatVT != axesVT):
         repeatHZ = axesHZ
         repeatVT = axesVT
-        if (axesVT > 0):
-            try:
-                myMotorRT.run(Adafruit_MotorHAT.FORWARD)
-                myMotorLT.run(Adafruit_MotorHAT.FORWARD)
-            except OSError:
-                mh = Adafruit_MotorHAT(addr=0x60)
-                print ("error error error error error")
-            axesMotorRT = axesVT
-            axesMotorLT = axesVT
-            if (axesHZ > 0):
-                axesMotorRT = (axesVT *(1- axesHZ))
-                print('right')
-            elif (axesHZ < 0):
-                axesMotorLT = (axesVT *(1- (axesHZ*(-1))))
-                print('left')
-            try:
-                myMotorLT.setSpeed(int(axesMotorLT))
-                myMotorRT.setSpeed(int(axesMotorRT))
-            except OSError:
-                mh = Adafruit_MotorHAT(addr=0x60)
-                print ("error error error error error")
-           # print ("LEFT: ", axesMotorLT)
-           # print ("RIGHT: ", axesMotorRT)
+        if (axesVT <= 0):
+            forward(axesVT, axesHZ)
+        elif (axesVT > 0):
+            reverse(axesVT, axesHZ)
 
-        elif (axesVT == 0 and axesHZ ==0):
-            myMotorRT.run(Adafruit_MotorHAT.RELEASE);
-            myMotorLT.run(Adafruit_MotorHAT.RELEASE);
-            print ("release")
-
-def forwar():
-    
-    pygame.event.pump()
-    
